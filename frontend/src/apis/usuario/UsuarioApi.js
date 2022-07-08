c6d2d4c25634699e0fa532d09ae0b9f0339cec35
Jessica Pereira <jc.moraespereira@gmail.com>
2022-07-08 10:59:22 -0300

    erro ao excluir usuario na tela admin

--- frontend/src/apis/usuario/UsuarioApi.js
+++ frontend/src/apis/usuario/UsuarioApi.js
@@ -4,4 +4,17 @@ export default {
     login: (body) => {
         return http.post(`/auth`, body)
     },
+
+    listarUsuario: () => {
+        return http.get(`/usuarios`);
+    },
+
+    salvarUsuario: (usuario, token) => {
+        return http.post(`/usuario`,usuario, { headers: {Authorization:`Bearer ${token}`}});
+    },
+
+    apagar: (idUser, token) => {
+        return http.delete(`/usuario/${idUser}`, { headers: {Authorization:`Bearer ${token}`}});
+    },
+    
 }
--- frontend/src/views/admin/Administrativo.vue
+++ frontend/src/views/admin/Administrativo.vue
@@ -3,30 +3,30 @@
     <NavBarAdmin />
     <v-card elevation="10">
       <v-card-title>
-        <span class="headline">Cadastrar Novo usuário</span>
+        <span class="headline">Cadastrar Novo Usuário</span>
       </v-card-title>
       <v-card-text>
         <v-container grid-list-md>
-          <v-form>
+          <v-form @submit.prevent="salvar">
             <v-layout wrap>
               <v-flex xs12>
                 <v-text-field
                   label="User*"
-                  v-model="imagemProduto"
+                  v-model="usuario.user"
                 ></v-text-field>
               </v-flex>
               <v-flex xs12>
                 <v-text-field
                   label="Senha*"
                   required
-                  v-model="categoriaProduto"
+                  v-model="usuario.senha"
                 ></v-text-field>
               </v-flex>
               <v-flex xs12>
                 <v-text-field
                   label="Tipo*"
                   required
-                  v-model="nomeProduto"
+                  v-model="usuario.tipo"
                 ></v-text-field>
               </v-flex>
             </v-layout>
@@ -37,7 +37,7 @@
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn color="error" small text @click="limparForm()">cancelar</v-btn>
-        <v-btn color="success" small>cadastrar</v-btn>
+        <v-btn color="success" small @click="salvar()">cadastrar</v-btn>
       </v-card-actions>
     </v-card>
     <v-card class="mt-5" elevation="14">
@@ -51,11 +51,12 @@
         dense
         class="pr-3 pl-3 mt-5 elevation-1 text-caption"
         :headers="cabecalho"
-        :items="items"
+        :items="listaDeUsuarios"
+        :key="usuario.id"
       >
         <template slot="no-data"> Nenhum usuário encontrado. </template>
 
-        <template class="text-caption" v-slot:[`item.action2`]="{ item }">
+        <template class="text-caption" v-slot:[`item.action`]="{ item }">
           <v-row justify="center" align-items="center">
             <v-tooltip bottom>
               <template v-slot:activator="{ on, attrs }">
@@ -64,6 +65,7 @@
                   color="error"
                   elevation="2"
                   small
+                  @click="deleteItem(item.idUser)"
                 >
                   mdi-delete
                 </v-icon>
@@ -79,11 +81,20 @@
 
 <script>
 import NavBarAdmin from "@/components/Navbar/NavBarAdmin.vue";
+import UsuarioApi from "@/apis/usuario/UsuarioApi";
 
 export default {
   data() {
     return {
+      listaDeUsuarios: [],
       cabecalho: [
+        {
+          text: "Id",
+          align: "center",
+          filterable: true,
+          divider: true,
+          value: "idUser",
+        },
         {
           text: "Usuário",
           align: "center",
@@ -102,11 +113,18 @@ export default {
           text: "Ações",
           align: "center",
           divider: true,
-          value: "action2",
+          value: "action",
         },
       ],
       user: [],
+      senha: [],
       tipo: [],
+      usuario: {
+        idUser: "",
+        user: "",
+        senha: "",
+        tipo: "",
+      },
     };
   },
 
@@ -115,7 +133,7 @@ export default {
   },
 
   created() {
-    this.inicializar();
+    this.listarUsuario();
   },
 
   methods: {
@@ -123,98 +141,74 @@ export default {
       this.$router.push({ name: "ResumoAdmin" });
     },
 
-    // deleteItem(idProduto) {
-    //   if (confirm("Tem certeza que deseja excluir esse produto?")) {
-    //     var accessToken = sessionStorage.getItem("accessToken");
-    //     var token = accessToken.replace(/"/gi, "");
+    deleteItem(idUser) {
+      if (confirm("Tem certeza que deseja excluir esse usuário?")) {
+        var accessToken = sessionStorage.getItem("accessToken");
+        var token = accessToken.replace(/"/gi, "");
 
-    //     ProdutoApi.apagar(idProduto, token)
-    //       .then((response) => {
-    //         response.data;
-    //         alert("Produto excluido com êxito");
-    //         this.listarProdutos();
-    //       })
+        UsuarioApi.apagar(idUser, token)
+          .then((response) => {
+            response.data;
+            alert("Usuário excluido com êxito");
+            this.listarProdutos();
+          })
 
-    //       .catch((error) => {
-    //         console.log(error);
-    //         alert("Erro ao excluir produto");
-    //       })
-    //       .finally(() => {});
-    //   }
-    // },
-
-    inicializar(){
-      this.items = [
-        {
-          user: "Admin",
-          tipo: "Administrador",
-        },
-         {
-          user: "Vitor",
-          tipo: "Atendente",
-        },
-         {
-          user: "Leon",
-          tipo: "Atendente",
-        },
-      ]
+          .catch((error) => {
+            console.log(error);
+            alert("Erro ao excluir usuário");
+          })
+          .finally(() => {});
+      }
     },
 
-    // listarProdutos() {
-    //   const listaDeProdutos = {
-    //     idProduto: this.idProduto,
-    //     categoriaProduto: this.categoriaProduto,
-    //     nomeProduto: this.nomeProduto,
-    //     descricaoProduto: this.descricaoProduto,
-    //     valorProduto: this.valorProduto,
-    //   };
+    listarUsuario() {
+      const listaDeUsuarios = {
+        idUser: this.idUser,
+        user: this.user,
+        tipo: this.tipo,
+      };
 
-    //   ProdutoApi.listarProdutos(listaDeProdutos).then((response) => {
-    //     response.data.forEach((item) => {
-    //       this.listaDeProdutos.push({
-    //         idProduto: item.idProduto,
-    //         categoriaProduto: item.categoriaProduto,
-    //         nomeProduto: item.nomeProduto,
-    //         descricaoProduto: item.descricaoProduto,
-    //         valorProduto: item.valorProduto,
-    //       });
-    //     });
-    //   });
-    // },
+      UsuarioApi.listarUsuario(listaDeUsuarios).then((response) => {
+        response.data.forEach((item) => {
+          this.listaDeUsuarios.push({
+            idUser: item.idUser,
+            user: item.user,
+            tipo: item.tipo,
+          });
+        });
+      });
+    },
 
-    // salvar() {
-    //   const produto = {
-    //     categoriaProduto: this.categoriaProduto,
-    //     nomeProduto: this.nomeProduto,
-    //     descricaoProduto: this.descricaoProduto,
-    //     valorProduto: this.valorProduto,
-    //   };
+    salvar() {
+      const usuario = {
+        user: this.user,
+        senha: this.senha,
+        tipo: this.tipo,
+      };
 
-    //   var accessToken = sessionStorage.getItem("accessToken");
-    //   var token = accessToken.replace(/"/gi, "");
-    //   ProdutoApi.salvar(this.produto, token)
-    //     .then((response) => {
-    //       console.log(response);
-    //       alert("Produto salvo com sucesso!");
-    //       this.listarProdutos();
-    //     })
+      var accessToken = sessionStorage.getItem("accessToken");
+      var token = accessToken.replace(/"/gi, "");
+      UsuarioApi.salvarUsuario(this.usuario, token)
+        .then((response) => {
+          console.log(response);
+          alert("Usuário salvo com sucesso!");
+          this.listarUsuario();
+        })
 
-    //     .catch((error) => {
-    //       console.log(error);
-    //       alert("Erro ao cadastrar novo produto");
-    //     })
-    //     .finally(() => {
-    //       this.limparForm();
-    //     });
-    // },
+        .catch((error) => {
+          console.log(error);
+          alert("Erro ao cadastrar novo usuário");
+        })
+        .finally(() => {
+          this.limparForm();
+        });
+    },
 
-    // limparForm() {
-    //   this.produto.imagemProduto = [];
-    //   this.produto.categoriaProduto = [];
-    //   this.produto.nomeProduto = [];
-    // },
-
-    
+    limparForm() {
+      this.novoUsuario.user = [];
+      this.novoUsuario.senha = [];
+      this.novoUsuario.tipo = [];
+    },
   },
 };
 </script>
